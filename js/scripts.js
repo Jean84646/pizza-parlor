76ad7a1c0634dc4e47367322b6bbc811b4bd4897
Jean Jia <jean84646@gmail.com>
2018-06-15 21:46:39 -0700

    Fix javascript requiring two clicks to display price.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // Business logic
-function Pizza(toppings, size, price){
+function Pizza(toppings, size, price) {
   this.arrayToppings = toppings;
   this.pizzaSize = size;
   this.price = 0;
@@ -12,38 +12,35 @@ Pizza.prototype.calculatePrice = function() {
   const STANDARD = 12;
   var extraCharge = 0;
 
-  if(arrayToppings.length > 1 && pizzaSize === 12){
+  if(arrayToppings.length > 1 && pizzaSize === 12) {
     extraCharge += 2;
   } else if(pizzaSize === 14) {
     extraCharge += 3;
-  } else if (pizzaSize === 16){
+  } else if (pizzaSize === 16) {
     extraCharge += 6;
   };
   return STANDARD + extraCharge;
 };
 
 // User interface logic
-$(document).ready(function(){
-  $("form#form-toppings").submit(function(event){
+$(document).ready(function() {
+  $("form#form-toppings").submit(function(event) {
     event.preventDefault();
-    $("input:checkbox[name=toppings]:checked").each(function(){
-      var toppings = $(this).val();
-      arrayToppings.push(toppings);
-    });
-    pizzaSize = parseInt($("input:radio[name=size]:checked").val());
-    var perPizza = new Pizza(arrayToppings, pizzaSize);
+      $("input:checkbox[name=toppings]:checked").each(function() {
+        var toppings = $(this).val();
+        arrayToppings.push(toppings);
+      });
+      pizzaSize = parseInt($("input:radio[name=size]:checked").val());
+      var perPizza = new Pizza(arrayToppings, pizzaSize);
 
-    $("button#add-pizza").click(function() {
       arrayToppings.forEach(function(item) {
         $("ul#itemize").append("<li>" + item + "</li>");
       });
 
       $("ul#itemize").append("<li>" + pizzaSize + ' "' + "</li>");
-      
+
       perPizza.price = perPizza.calculatePrice();
       $("#total-price").text(perPizza.price);
       $("#output-area").show();
-      });
-
   });
 });
